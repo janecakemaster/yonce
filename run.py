02ee1e8ac3cf8bd153b5454de89c7717121e66fc
janecakemaster <janecakemaster@gmail.com>
2014-02-08 10:43:08 -0500

    pivoting

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from random import randint
 
 app = Flask(__name__)
 
-songs = ["drunkinlove", "loveontop", "runtheworld", "singleladies", "halo",
-         "sweetdreams", "crazyinlove", "irreplacable"]
+songs = ["misscarter", "womp", "ifiwereaboy", "memyselfandi", "irreplacable",
+         "singleladies", "freakumdress", "diva", "runtheworld", "flawless"]
 bucket_url = "https://s3.amazonaws.com/yonce/"
 
 
@@ -14,78 +14,44 @@ def start_call():
     """Welcome caller and present menu of songs."""
 
     resp = twilio.twiml.Response()
-    resp.say("THIS IS THE GREETING FOR THE CALLER")
-    resp.say("more stuff")
-    resp.say("Listen up for the menu options")
-    # Play an MP3
+    resp.say("Who's the better half: Beyonsay or Jay zee?")
     with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
-        g.say("""Press 1 for a random song.
-                 Press 2 for Drunk in Love.
-                 Press 3 for Love on Top.
-                 Press 4 for Run The World (Girls).
-                 Press 5 for Single Ladies (Put A Ring On It).
-                 Press 6 for Halo.
-                 Press 7 for Sweet Dreams.
-                 Press 8 for Crazy In Love (Featuring Jay-Z).
-                 Press 9 for Irreplacable.
-                 To hear the menu again, wait or press 0.""")
+        g.say("""Press 1 for Beyonce-ay.
+                 Press 2 for Jay zee.""")
     return str(resp)
 
 
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
-    """Handle key press from a user."""
+    """Get fierceness rating."""
 
     digit_pressed = request.values.get('Digits', None)
     if digit_pressed == "1":
         resp = twilio.twiml.Response()
-        rando = random.randint(0, 7)
-        resp.play(bucket_url + songs[rando] + ".mp3")
+        with resp.gather(numDigits=1, action="/pick-song", method="POST") as g:
+            g.say("""On a scale of one to nine, how fierce do you feel today?
+                     Select a number from 1 to 9.""")
         return str(resp)
-
     elif digit_pressed == "2":
         resp = twilio.twiml.Response()
         resp.play(bucket_url + songs[0] + ".mp3")
         return str(resp)
+    else:
+        return redirect("/")
 
-    elif digit_pressed == "3":
+
+@app.route("/pick-song", methods=['GET', 'POST'])
+def pick_song():
+    if digit_pressed == "1":
         resp = twilio.twiml.Response()
         resp.play(bucket_url + songs[1] + ".mp3")
         return str(resp)
-
-    elif digit_pressed == "4":
+    elif digit_pressed == "2":
         resp = twilio.twiml.Response()
         resp.play(bucket_url + songs[2] + ".mp3")
         return str(resp)
 
-    elif digit_pressed == "5":
-        resp = twilio.twiml.Response()
-        resp.play(bucket_url + songs[3] + ".mp3")
-        return str(resp)
-
-    elif digit_pressed == "6":
-        resp = twilio.twiml.Response()
-        resp.play(bucket_url + songs[4] + ".mp3")
-        return str(resp)
-
-    elif digit_pressed == "7":
-        resp = twilio.twiml.Response()
-        resp.play(bucket_url + songs[5] + ".mp3")
-        return str(resp)
-
-    elif digit_pressed == "8":
-        resp = twilio.twiml.Response()
-        resp.play(bucket_url + songs[6] + ".mp3")
-        return str(resp)
-
-    elif digit_pressed == "9":
-        resp = twilio.twiml.Response()
-        resp.play(bucket_url + songs[7] + ".mp3")
-        return str(resp)
-
-    # If the caller pressed anything but 1, redirect them to the homepage.
-    else:
-        return redirect("/")
+    return str(resp)
 
 
 if __name__ == "__main__":
