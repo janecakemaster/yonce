0281f12e50ae7e4b954c6298bb776c88740124c4
janecakemaster <janecakemaster@gmail.com>
2014-02-08 09:51:05 -0500

    Revert "Revert "debugss""
    This reverts commit 8ea6e35fab842bca9da72504ac016d1d3c4a37b7.

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def hello_monkey():
     resp.say("Do you identify with Beyonce-ay or Jay Zeeeee?")
     # Play an MP3
     #resp.play("https://s3.amazonaws.com/yonce/upgradeu.mp3")
-    resp.record(maxLength="2", transcribeCallback="/transcribed", action="/handle-recording")
+    resp.record(maxLength="2", transcribeCallback="/transcribed")
     return str(resp)
 
 
